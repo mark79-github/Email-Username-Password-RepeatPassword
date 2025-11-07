04df378cd81174b40d0c47c7b4458650ec132937
mark79-github <52877024+mark79-github@users.noreply.github.com>
2025-11-07 05:42:30 +0200

    Update express-handlebars usage with the latest version changes

--- config/express.js
+++ config/express.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const handlebars = require('express-handlebars');
+const { engine } = require('express-handlebars');
 const cookieParser = require('cookie-parser');
 const {isAuthenticated} = require('../middlewares');
 
 module.exports = (app) => {
-    app.engine('hbs', handlebars({extname: 'hbs'})); // template-engine
+    app.engine('hbs', engine({extname: 'hbs'})); // template-engine
     app.set('view engine', 'hbs'); // view-engine
     app.use(express.static('static')); // static folder
     app.use(express.urlencoded({extended: true})); // body-parser
